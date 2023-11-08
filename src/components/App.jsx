f04595c356269c61c0e38b3fa18ac40dc72d5763
nikitkachan <nikyc777@gmail.com>
2023-11-08 12:05:03 +0200

    добавил notiflix

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,16 @@
-import { useState, useEffect } from 'react'
-import { StyledApp } from './App.styled'
-import { SearchBar } from './Searchbar/Searchbar'
-import { fetchImages } from './Api/api'
-import { ImageGallery } from './ImageGallery/ImageGallery'
-import { ImageGalleryItem } from './ImageGalleryItem/ImageGalleryItem'
-import Loader from './Loader/Loader'
-import Button from './Button/Button'
-import { Modal } from './Modal/Modal'
-import { animateScroll } from 'react-scroll'
+import { useState, useEffect } from 'react';
+import { StyledApp } from './App.styled';
+import { SearchBar } from './Searchbar/Searchbar';
+import { fetchImages } from './Api/api';
+import { ImageGallery } from './ImageGallery/ImageGallery';
+import { ImageGalleryItem } from './ImageGalleryItem/ImageGalleryItem';
+import Loader from './Loader/Loader';
+import Button from './Button/Button';
+import { Modal } from './Modal/Modal';
+import { animateScroll } from 'react-scroll';
+import Notiflix from 'notiflix';
 
 export const App = () => {
-  
   const [searchWord, setSearchWord] = useState(null);
   const [images, setImages] = useState([]);
   const [page, setPage] = useState(1);
@@ -19,50 +19,54 @@ export const App = () => {
   const [isOpenModal, setIsOpenModal] = useState(false);
   const [modalData, setModalData] = useState(null);
   const [error, setError] = useState(null);
-
-  
+  const [totalHits, setTotalHits] = useState(null);
 
   useEffect(() => {
     if (!searchWord) {
       return;
-    };
-    const fetchImgs = async () => {
-    try {
-      setIsLoading(true);
-      setIsShown(false);
-      
-      const result = await fetchImages(searchWord, page)
-     
-      if (page>1) {
-        setImages(prev => [...prev, ...result.hits]);
-        setIsShown(true);
-      } else {
-        setImages([...result.hits]);
-        setIsShown(true);
-      };
-      
-      if (result.hits.length <= 11) {
-        setIsShown(false)
-      };
-    } catch (error) {
-      setError(error.message);
-    } finally {
-      setIsLoading(false);
     }
-  };
-    fetchImgs();
- 
-  }, [searchWord, page]);
+    const fetchImgs = async () => {
+      try {
+        setIsLoading(true);
+        setIsShown(false);
 
-  // useEffect(() => {
-  //   if (images.length>12) {
-  //     window.scrollBy({
-  //         top: 500,
-  //         behavior: "smooth",
-  //       })
-  //   } 
-  // }, [images]);
-  
+        const result = await fetchImages(searchWord, page);
+
+        if (page > 1) {
+          setImages(prev => [...prev, ...result.hits]);
+          setIsShown(true);
+          setTotalHits(result.totalHits);
+        } else {
+          setImages([...result.hits]);
+          setIsShown(true);
+          setTotalHits(result.totalHits);
+        }
+
+        if (result.hits.length <= 11) {
+          setIsShown(false);
+        }
+      } catch (error) {
+        setError(error.message);
+      } finally {
+        setIsLoading(false);
+      }
+    };
+    fetchImgs();
+  }, [searchWord, page, totalHits]);
+
+  useEffect(() => {
+    if (totalHits !== null && totalHits !== 0) {
+      Notiflix.Notify.success(`We found ${totalHits} images!`, {
+        position: 'center-center',
+      });
+    }
+    if (totalHits === 0) {
+      Notiflix.Notify.failure(`Sorry, we didn't found any images!`, {
+        position: 'center-center',
+      });
+    }
+  }, [totalHits]);
+
   const openModal = data => {
     setIsOpenModal(true);
     setModalData(data);
@@ -78,8 +82,8 @@ export const App = () => {
     setPage(1);
     setImages([]);
   };
-    
-const scrollOnMoreButton = () => {
+
+  const scrollOnMoreButton = () => {
     animateScroll.scrollToBottom({
       duration: 1000,
       delay: 10,
@@ -92,28 +96,24 @@ const scrollOnMoreButton = () => {
     scrollOnMoreButton();
   };
 
-    return (
-      <StyledApp >
-        <SearchBar onSubmit={onSubmit} />
-         {error !== null && (
-          <p className="error-bage">
-            Oops, some error occured... Error message: {error}
-          </p>
+  return (
+    <StyledApp>
+      <SearchBar onSubmit={onSubmit} />
+      {error !== null && (
+        <p className="error-bage">
+          Oops, some error occured... Error message: {error}
+        </p>
+      )}
+      <ImageGallery>
+        {images !== [] && (
+          <ImageGalleryItem data={images} openModal={openModal} />
         )}
-        <ImageGallery>
-          {images !== [] && <ImageGalleryItem data={images} openModal={openModal} />}
-        </ImageGallery> 
-         {isLoading && <Loader />}
-        {images !== [] && isShown && <Button onLoadMoreHandler={onLoadMoreHandler} />}
-        {isOpenModal && (
-          <Modal
-            closeModal={closeModal}
-            modalData={modalData}
-          />
-        )}
-      </StyledApp>
-    )
-  
+      </ImageGallery>
+      {isLoading && <Loader />}
+      {images !== [] && isShown && (
+        <Button onLoadMoreHandler={onLoadMoreHandler} />
+      )}
+      {isOpenModal && <Modal closeModal={closeModal} modalData={modalData} />}
+    </StyledApp>
+  );
 };
-
-
--- src/components/Searchbar/Searchbar.jsx
+++ src/components/Searchbar/Searchbar.jsx
@@ -1,38 +1,36 @@
-
 import { BsSearch } from 'react-icons/bs';
 import { StyledSearchBar } from './Searchbar.styled';
 import Notiflix from 'notiflix';
 
 export const SearchBar = ({ onSubmit }) => {
-    
-   
-    const handleInputSubmit = (e) => {
-        e.preventDefault();
-        const searchWord = e.currentTarget[1].value;
-        if (searchWord.trim() === "") {
-            Notiflix.Notify.failure('Please, enter something!');
-            return
-        }
-    
-        onSubmit(searchWord);
+  const handleInputSubmit = e => {
+    e.preventDefault();
+    const searchWord = e.currentTarget[1].value;
+    if (searchWord.trim() === '') {
+      Notiflix.Notify.failure('Please, enter something!', {
+        position: 'center-center',
+      });
+      return;
     }
 
-    return (
-        <StyledSearchBar>
-            <form onSubmit={handleInputSubmit} className="form">
-                <button type="submit" className="button">
-                <BsSearch />
-                </button>
+    onSubmit(searchWord);
+  };
 
-                <input
-                className="input"
-                type="text"
-                autoComplete="off"
-                autoFocus
-                placeholder="Search images and photos"
-                />
-            </form>
-      </StyledSearchBar>
-    )
-  }
+  return (
+    <StyledSearchBar>
+      <form onSubmit={handleInputSubmit} className="form">
+        <button type="submit" className="button">
+          <BsSearch />
+        </button>
 
+        <input
+          className="input"
+          type="text"
+          autoComplete="off"
+          autoFocus
+          placeholder="Search images and photos"
+        />
+      </form>
+    </StyledSearchBar>
+  );
+};
